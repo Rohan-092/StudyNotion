844e032c6524be00033c901c088d29704a1a8090
Rohan Garg <119299125+Rohan-092@users.noreply.github.com>
2026-02-07 17:41:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: 'https://study-notion-dupl-eight.vercel.app',
+        origin: 'https://studynotiondupl-1.onrender.com',
         credentials: true,
     })
 )
