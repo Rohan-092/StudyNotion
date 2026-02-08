8ab3de1bc961c61313f94c83551ca797c0cae6bd
Rohan Garg <119299125+Rohan-092@users.noreply.github.com>
2026-02-08 15:12:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: 'https://study-notion-dupl.vercel.app',
+        origin: 'http://localhost:3000',
         credentials: true,
     })
 )
