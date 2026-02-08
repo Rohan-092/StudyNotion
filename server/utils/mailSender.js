57e36be9a3719a2d3d1ea1fe2b2724c8434bd9a8
Rohan Garg <gargrohan100902@gmail.com>
2026-02-08 23:08:36 +0530

    Update mailSender.js
    syntax error in from

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -14,7 +14,7 @@ const mailSender = async (email, title, body) => {
     });
   
     const info = await transporter.sendMail({
-      from: `"my app" <${process.env.MAIL_USER}>,
+      from: `"my app" <${process.env.MAIL_USER}>`,
       to: email,
       subject: title,
       html: body,
