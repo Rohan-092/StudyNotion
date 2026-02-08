103747b96a717c86ed774357179cf835c8f3d654
Rohan Garg <119299125+Rohan-092@users.noreply.github.com>
2026-02-08 14:50:04 +0530

    Update ResetPasswordurl.js

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -30,7 +30,7 @@ exports.resetPasswordToken = async(req,res)=>{
                                             }, {new:true});
 
         //create url
-        const url = `https://studynotion1-gvt6.onrender.com/updatePassword/${token}`;
+        const url = `https://study-notion-dupl.vercel.app/updatePassword/${token}`;
 
         // send email containing the url
         try{
