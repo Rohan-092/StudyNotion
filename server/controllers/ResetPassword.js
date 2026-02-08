6d8f8e9cc1b7d17519d1513529b167d5f6c4686c
Rohan Garg <119299125+Rohan-092@users.noreply.github.com>
2026-02-08 11:36:27 +0530

    Update ResetPassword.js

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -30,7 +30,7 @@ exports.resetPasswordToken = async(req,res)=>{
                                             }, {new:true});
 
         //create url
-        const url = `https://studynotiondupl-1.onrender.com/updatePassword/${token}`;
+        const url = `http://localhost:3000/updatePassword/${token}`;
 
         // send email containing the url
         try{
