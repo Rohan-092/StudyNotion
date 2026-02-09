748c03f98f6aa26b69b4cd4e0798ac1f38df13c5
Rohan Garg <gargrohan100902@gmail.com>
2026-02-09 12:35:46 +0530

    Update OTP.js

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -13,25 +13,25 @@ const OTPSchema = mongoose.Schema({
     },
     createdAt:{
         type:Date,
-        default:Date.now(), 
+        default:Date.now, 
         expires:5*60,
     },
 });
 
-// async function sendVerificationEmail(email,otp){
-//     try{
-//         const mailResponse = await mailSender(email,"Verification email from StudyNotation",otpTemplate(otp));
-//         console.log("email sent successfully",mailResponse);
-//     }
-//     catch(error){
-//         console.log("error occured while sending email",error);
-//         throw error;
-//     }
-// }
+async function sendVerificationEmail(email,otp){
+    try{
+        const mailResponse = await mailSender(email,"Verification email from StudyNotation",otpTemplate(otp));
+        console.log("email sent successfully",mailResponse);
+    }
+    catch(error){
+        console.log("error occured while sending email",error);
+        throw error;
+    }
+}
 
-// OTPSchema.pre("save", async function(next){
-//     await sendVerificationEmail(this.email,this.otp);
-//     next();
-// })
+OTPSchema.pre("save", async function(next){
+    await sendVerificationEmail(this.email,this.otp);
+    next();
+})
 
 module.exports = mongoose.model("OTP",OTPSchema);
